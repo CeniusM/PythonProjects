4d6fd7fc87a0d11e79392995a040d1658d7cb46e
CeniusM <cenius.meier@gmail.com>
2022-09-29 12:53:53 +0200

    changes

--- SAP/RandomSAPPlayer.py
+++ SAP/RandomSAPPlayer.py
@@ -3,9 +3,7 @@ from random import randint
 
 def Click(x,y) -> None:
     pyautogui.moveTo(x,y)
-    #win32api.SetCursorPos((x,y))
-    # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,20,200,0,0)
-    # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,20,200,0,0)
+
     
 def Rand(min: int = 0, max: int = 1) -> int:
     return randint(min, max)
@@ -25,10 +23,20 @@ def main():
     for _ in range(10):
         x,y = poses[Rand(0, len(poses)-1)]
         Click(x,y)
-    # Click(endTurnPos[0], endTurnPos[1])
-    Click(0,0)
-    Click(poses[0][0], poses[0][1])
+    Click(endTurnPos[0], endTurnPos[1])
+    
+def PrintCoords():
+    print('Press Ctrl-C to quit.')
+    try:
+        while True:
+            x, y = pyautogui.position()
+            positionStr = f"X: {str(x).rjust(4)} Y: {str(y).rjust(4)}"
+            print(positionStr, end='')
+            print('\b' * len(positionStr), end='', flush=True)
+    except KeyboardInterrupt:
+        print('\n')
 
     #1535, 863 is apparently the real range on the screen
 if __name__ == "__main__":
+    # PrintCoords()
     main()
