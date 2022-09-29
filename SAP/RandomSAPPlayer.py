12e344240693741bc3e76bc216074377c51b4397
Tr4shL0rd <ketil04@gmail.com>
2022-09-29 12:36:59 +0200

    fixed

--- SAP/RandomSAPPlayer.py
+++ SAP/RandomSAPPlayer.py
@@ -1,46 +1,34 @@
-from pydoc import cli
-import win32api, win32con
-from time import sleep as Sleep
+import pyautogui
 from random import randint
 
-def Click(x,y):
-    win32api.SetCursorPos((x,y))
+def Click(x,y) -> None:
+    pyautogui.moveTo(x,y)
+    #win32api.SetCursorPos((x,y))
     # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,20,200,0,0)
     # win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,20,200,0,0)
     
-def Rand(min: int = 0, max: int = 1):
+def Rand(min: int = 0, max: int = 1) -> int:
     return randint(min, max)
+def main():
+    #SAP poses
+    poses = [(530,444) ,(666,444) ,(814,444) ,(960,444) ,(1111,444),(530,700),(666,700),(814,700),(960,700),(1111,700),(1255,700),(1400,700),(328,1000)]
 
-#SAP poses
-poses = []
-# active pets
-poses.append({530,444})
-poses.append({666,444})
-poses.append({814,444})
-poses.append({960,444})
-poses.append({1111,444})
-# shop pets/food
-poses.append({530,700})
-poses.append({666,700})
-poses.append({814,700})
-poses.append({960,700})
-poses.append({1111,700})
-poses.append({1255,700})
-poses.append({1400,700})
-# roll
-poses.append({328,1000})
+    # active pets 
+    # shop pets/food
+    # roll
 
-# end turn
-endTurnPos = {1500, 1000}
+    # end turn
+    endTurnPos = (1500, 1000)
 
-# while(True):
-#     print(win32api.GetCursorPos())
-for _ in range(10):
-    x,y = poses[Rand(0, poses.__len__() - 1)]
-    Click(x,y)
-# Click(endTurnPos[0], endTurnPos[1])
-Click(0,0)
+    # while(True):
+    #     print(win32api.GetCursorPos())
+    for _ in range(10):
+        x,y = poses[Rand(0, len(poses)-1)]
+        Click(x,y)
+    # Click(endTurnPos[0], endTurnPos[1])
+    Click(0,0)
+    Click(poses[0][0], poses[0][1])
 
-Click(poses[0][0], poses[0][1])
-
-#1535, 863 is apparently the real range on the screen
+    #1535, 863 is apparently the real range on the screen
+if __name__ == "__main__":
+    main()
